9bc6714fc29ccce6d8384a404bc08b40a519c05a
lmohin <loic.mohin@gmail.com>
2025-07-18 18:10:05 +0200

    started to clean here

--- linear.py
+++ linear.py
@@ -2,41 +2,42 @@ from pandas import read_csv
 import csv
 import matplotlib.pyplot as plt
 
+def standardizeDatas(df):
+    newDf = df.assign(
+        km = (df['km'] - df['km'].mean()) / df['km'].std(),
+        price = (df['price'] - df['price'].mean()) / df['price'].std()
+    )
+    return newDf
+
+
 def linear_regretion():
     df = read_csv('data.csv')
+    standardDf = standardizeDatas(df)
+    print(standardDf)
     mu_x = df['km'].mean()
     sigma_x = df['km'].std()
     mu_y = df['price'].mean()
     sigma_y = df['price'].std() 
     plt.scatter(df['km'], df['price'])
-    df['km'] = (df['km'] - df['km'].mean()) / df['km'].std()
-    df['price'] = (df['price'] - df['price'].mean()) / df['price'].std()
     a = 0
     b = 0
-    def to_minimize(row):
-        return ((row['km'] * a + b - row['price']) ** 2)
     for i in range(10000):
         def a_min(row):
             return ((row['km'] * a + b - row['price']) * row['km'])
         def b_min(row):
             return ((row['km'] * a + b - row['price']))
-        tmp_a = df.apply(a_min, axis=1).sum()
+        tmp_a = standardDf.apply(a_min, axis=1).sum()
         tmp_a = (tmp_a / df.shape[0]) * 0.01
-        tmp_b = df.apply(b_min, axis=1).sum()
+        tmp_b = standardDf.apply(b_min, axis=1).sum()
         tmp_b = (tmp_b / df.shape[0]) * 0.01
         a -= tmp_a
         b -= tmp_b
-        print(a,b)
-    print(a * sigma_y / sigma_x, b * sigma_y + mu_y - a * sigma_y / sigma_x  * mu_x)
-    print(df.apply(to_minimize, axis=1))
-    aa = a * sigma_y / sigma_x
-    bb = b * sigma_y + mu_y - a * sigma_y / sigma_x * mu_x
-    aas = str(aa)
-    bbs = str(bb)
+    realA = a * sigma_y / sigma_x
+    realB = b * sigma_y + mu_y - a * sigma_y / sigma_x * mu_x
     with open("values.csv", "w", newline='') as valuesFile:
         writer = csv.writer(valuesFile)
         writer.writerow(['a', 'b'])
-        writer.writerow([aas, bbs])
-    plt.plot([0, 250000], [aa*0 + bb, aa*250000 + bb], 'r-', lw=2)
+        writer.writerow([realA, realB])
+    plt.plot([0, 250000], [realA*0 + realB, realA*250000 + realB], 'r-', lw=2)
     plt.show()
 linear_regretion()
