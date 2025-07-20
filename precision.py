4cb8dc93a42d53feb769478ab27e375227ea2ba3
lmohin <loic.mohin@gmail.com>
2025-07-20 20:11:30 +0200

    determination coefficient calculated

--- precision.py
+++ precision.py
@@ -2,6 +2,11 @@ import csv
 from prediction import getValues
 from pandas import read_csv
 
+def calculate_mean_diff(df):
+    mu = df['price'].mean()
+    def squared_diff(row):
+        return((row['price'] - mu) ** 2)
+    return (df.apply(squared_diff, axis=1).sum())
 
 def calculate_precision():
     a,b = getValues()
@@ -12,8 +17,9 @@ def calculate_precision():
     except:
         print("Error: can not read data.csv")
         return
-    print(df.apply(squared_diff, axis=1).sum() / 2)
-    print(a,b)
+    d = df.apply(squared_diff, axis=1).sum()
+    c = calculate_mean_diff(df)
+    print(1 - d / c)
 
 if __name__ == "__main__":
     calculate_precision()
