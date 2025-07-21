8dcd57284ad0c7c8d9fa485f9ac9821d474d6423
lmohin <loic.mohin@gmail.com>
2025-07-21 15:24:17 +0200

    cleaned a bit and added doc

--- prediction.py
+++ prediction.py
@@ -1,6 +1,8 @@
 import csv
 
 def getValues():
+    """Returns the values stored in 'values.csv'"""
+
     try:
         with open("values.csv") as file:
             fileIterator = csv.reader(file)
@@ -11,18 +13,24 @@ def getValues():
         print("Can't read values, setting them to base values")
         return (0,0)
 
-def calculate(a, b, x):
+def affineFunction(a, b, x):
+    """Returns the result of the affine function y = ax + b"""
+    
     return (a * x + b)
 
 def predict():
-    values = getValues()
+    """Prompts the user to enter a mileage, then calculates the estimated price of the car"""
+    
+    a,b = getValues()
     try:
         weight = float(input("Please enter the mileage of the car you want to predict the price of:\n"))
     except:
         print("Please enter a correct float value")
         return
-    price = calculate(values[0], values[1], weight)
+    price = affineFunction(a, b, weight)
     print(f"The estimated price of the car is {price}")
+    if (price < 0):
+        print("Why would you buy this garbage car")
 
 if __name__ == "__main__":
     predict()
