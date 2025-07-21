690134063a9759b1dcaa2193c03005dedddc2642
lmohin <loic.mohin@gmail.com>
2025-07-21 15:45:42 +0200

    cleaned the calculus and added documentation

--- precision.py
+++ precision.py
@@ -1,25 +1,28 @@
-import csv
 from prediction import getValues
 from pandas import read_csv
 
-def calculate_mean_diff(df):
-    mu = df['price'].mean()
-    def squared_diff(row):
-        return((row['price'] - mu) ** 2)
-    return (df.apply(squared_diff, axis=1).sum())
+def calculateMeanDiffSum(df):
+    """Takes a dataframe as parameter, and calculates the sum of each values distance's to the mean value"""
 
-def calculate_precision():
+    mu = df['price'].mean()
+    def squaredDiff(row):
+        return((row['price'] - mu) ** 2)
+    return (df.apply(squaredDiff, axis=1).sum())
+
+def calculatePrecision():
+    """Calculates the determination coefficient r2 of a prediction model stored in 'values.csv'"""
     a,b = getValues()
-    def squared_diff(row):
+    def squaredDiff(row):
         return ((row['km'] * a + b - row['price']) ** 2)
     try:
         df = read_csv('data.csv')
     except:
         print("Error: can not read data.csv")
         return
-    d = df.apply(squared_diff, axis=1).sum()
-    c = calculate_mean_diff(df)
-    print(1 - d / c)
+    residualsSum = df.apply(squaredDiff, axis=1).sum()
+    meanDiffSum = calculateMeanDiffSum(df)
+    r2 = 1 - residualsSum / meanDiffSum
+    print(f"The dertemination coefficient r2 of this modele is {r2}")
 
 if __name__ == "__main__":
-    calculate_precision()
+    calculatePrecision()
